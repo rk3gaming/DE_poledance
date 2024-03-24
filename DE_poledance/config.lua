06b3c731eaf686dbf4641c1288c63f43cf99c709
fusion-develops <145299854+fusion-develops@users.noreply.github.com>
2024-03-24 00:37:06 -0400

    Update config.lua
    I saw i fucked up on something this fixes it

--- DE_poledance/config.lua
+++ DE_poledance/config.lua
@@ -1,19 +1,17 @@
 Config = {}
 
 Config.StripPoles = {
-    -- Vanilla Unicorn
-    {coords = vector3(109.1771, -1289.1078, 29.2497)},
-    {coords = vector3(102.2357, -1290.1837, 29.2497)},
-    {coords = vector3(104.7980, -1294.4106, 29.2497)},
-    -- Gabz Bahama Mamas
-    {coords = vector3(-1393.9738, -612.6129, 30.7029)},
-    {coords = vector3(-1390.9108, -616.5022, 30.7029)},
-    {coords = vector3(-1387.8596, -621.0838, 30.7029)},
-    {coords = vector3(-1391.0577, -632.1137, 31.3034)}
+    vector3(109.1771, -1289.1078, 29.2497),
+    vector3(102.2357, -1290.1837, 29.2497),
+    vector3(104.7980, -1294.4106, 29.2497),
+    vector3(-1393.9738, -612.6129, 30.7029),
+    vector3(-1390.9108, -616.5022, 30.7029),
+    vector3(-1387.8596, -621.0838, 30.7029),
+    vector3(-1391.0577, -632.1137, 31.3034)
 }
 
 Config.Anims = {
-    [1] = {dict = 'mini@strip_club@pole_dance@pole_dance1', clip = 'pd_dance_01'},
-    [2] = {dict = 'mini@strip_club@pole_dance@pole_dance2', clip = 'pd_dance_02'},
-    [3] = {dict = 'mini@strip_club@pole_dance@pole_dance3', clip = 'pd_dance_03'},
+    {dict = 'mini@strip_club@pole_dance@pole_dance1', clip = 'pd_dance_01'},
+    {dict = 'mini@strip_club@pole_dance@pole_dance2', clip = 'pd_dance_02'},
+    {dict = 'mini@strip_club@pole_dance@pole_dance3', clip = 'pd_dance_03'},
 }
